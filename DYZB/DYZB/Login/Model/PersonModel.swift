cd1e4c73f7b83e0216076d7ec52c8da3643f6aab
liangxinchang <lxc_network@163.com>
2019-11-15 15:29:03 +0800

    登录界面接口完成

---
+++ DYZB/DYZB/Login/Model/PersonModel.swift
@@ -0,0 +1,13 @@
+//
+//  PersonModel.swift
+//  DYZB
+//
+//  Created by liangxinchang on 2019/11/15.
+//  Copyright © 2019 liangxinchang. All rights reserved.
+//
+
+import UIKit
+
+class PersonModel: NSObject {
+
+}
